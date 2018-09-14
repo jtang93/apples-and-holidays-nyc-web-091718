d56676f9d556be0d7fad796662a0b0b23d1c5a81
jtang93 <tang.jonathan93@gmail.com>
2018-09-14 12:00:00 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |name, supply|
-      puts " #{name.to_s.capitalize}: #{supply.join(", ")"
+      puts " #{name.to_s.capitalize}: #{supply.join(", ")}"
       
     end
   end
