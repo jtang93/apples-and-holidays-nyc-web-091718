0d409d704e93c33905201623e58ae62f7a873305
jtang93 <tang.jonathan93@gmail.com>
2018-09-14 12:36:45 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -91,7 +91,7 @@ def all_holidays_with_bbq(holiday_hash)
     newarray << newhash.keys
       end
 
-    newarray.reject!{|item| item.empty?}
+    newarray.reject!{|item| item.empty?}.flatten
 end
 
 
